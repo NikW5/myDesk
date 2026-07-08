a3ff85f4c415f5761e8542adf59ef1b5bd0b4a43
NikW5 <jannikwendler@web.de>
2026-07-08 18:54:18 +0200

    issue 3: dashboard -> buttons eingefügt, layout angepasst

--- src/main/java/com/psyduck/myDesk/benutzerschnittstelle/DashboardView.java
+++ src/main/java/com/psyduck/myDesk/benutzerschnittstelle/DashboardView.java
@@ -1,21 +1,65 @@
 package com.psyduck.myDesk.benutzerschnittstelle;
 
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
-@Route("Dashboard")
+@Route("dashboard")
 public class DashboardView extends VerticalLayout{
 
-	private Button loginButton = new Button("LoginPage");
+	private Button logoutButton = new Button("Abmelden");
+
+    private HorizontalLayout headerLayout = new HorizontalLayout();
+    private VerticalLayout bodyLayout = new VerticalLayout();
 	
 	public DashboardView() {
-		add(new H1("Dashboard"));
-		add(loginButton);
+		setSizeFull();
 		
-		loginButton.addClickListener(event -> UI.getCurrent().navigate(LoginView.class));
+		headerLayout.setPadding(true);
+		headerLayout.setWidthFull();
+		headerLayout.add(new H1("Dashboard"), logoutButton);
+		headerLayout.addToEnd(logoutButton);
+
+		bodyLayout.setPadding(true);
+		bodyLayout.setWidthFull();
+		bodyLayout.setAlignItems(FlexComponent.Alignment.CENTER);
+		bodyLayout.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
 		
+		expand(bodyLayout); // gibt bodyLayout gesamten verbleibenden Platz auf der Seite
+		
+		
+		logoutButton.addClickListener(event -> UI.getCurrent().navigate(LoginView.class));
+		
+		add(headerLayout);
+		add(createDashboardNavigation());
 	}
+	
+	private Component createDashboardNavigation() {
+		HorizontalLayout obereButtons = new HorizontalLayout();
+		HorizontalLayout untereButtons = new HorizontalLayout();
+		Button nachrichtenButton = new Button("Nachrichten(2)");
+		Button chatButton = new Button("Chat");
+		Button kalenderButton = new Button("Kalender");
+		Button toDoButton = new Button("To-Dos");
+		
+		nachrichtenButton.setWidth("200px");
+		chatButton.setWidth("200px");
+		kalenderButton.setWidth("200px");
+		toDoButton.setWidth("200px");
+		
+		obereButtons.add(nachrichtenButton, chatButton);
+		untereButtons.add(kalenderButton, toDoButton);
+		
+		obereButtons.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+		untereButtons.setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+		
+		bodyLayout.add(obereButtons, untereButtons);
+		return bodyLayout;
+	}
+
 }
