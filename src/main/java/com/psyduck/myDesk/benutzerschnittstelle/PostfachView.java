4146fb019eecd0e6a049a58d752c53be6ee9c0df
NikW5 <jannikwendler@web.de>
2026-07-28 17:09:33 +0200

    Issue #18 - dynamischer Nachrichtenzähler im Dashboard angelegt

--- src/main/java/com/psyduck/myDesk/benutzerschnittstelle/PostfachView.java
+++ src/main/java/com/psyduck/myDesk/benutzerschnittstelle/PostfachView.java
@@ -8,6 +8,7 @@ import com.vaadin.flow.router.Route;
 import java.time.LocalDateTime;
 
 import com.psyduck.myDesk.persistenz.Nachricht;
+import com.psyduck.myDesk.persistenz.NachrichtService;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.masterdetaillayout.MasterDetailLayout;
 import com.vaadin.flow.component.textfield.TextArea;
@@ -52,59 +53,9 @@ public class PostfachView extends VerticalLayout {
                 .setHeader("Empfangen am")
                 .setFlexGrow(2);
 
-        grid.setSizeFull();
+        grid.setSizeFull();  
         
-        
-        // Dummy-Daten Anfang -----------------------------------------------------------------------------
-        Nachricht dummy1 = new Nachricht();
-        dummy1.setBenutzer("Max Mustermann");
-        dummy1.setTitel("Willkommen bei myDesk");
-        dummy1.setVorschau("Herzlich willkommen bei myDesk...");
-        dummy1.setInhalt("""
-                Hallo,
-
-                herzlich willkommen bei myDesk!
-
-                Wir freuen uns, dass Sie unser System nutzen.
-                Bei Fragen steht Ihnen der Support jederzeit zur Verfügung.
-
-                Viele Grüße
-                Ihr myDesk-Team
-                """);
-        dummy1.setEmpfangenAm(LocalDateTime.now().minusHours(2));
-
-        Nachricht dummy2 = new Nachricht();
-        dummy2.setBenutzer("IT-Support");
-        dummy2.setTitel("Passwort geändert");
-        dummy2.setVorschau("Ihr Passwort wurde erfolgreich geändert...");
-        dummy2.setInhalt("""
-                Guten Tag,
-
-                Ihr Passwort wurde erfolgreich geändert.
-
-                Sollten Sie diese Änderung nicht selbst vorgenommen haben,
-                wenden Sie sich bitte umgehend an den IT-Support.
-                """);
-        dummy2.setEmpfangenAm(LocalDateTime.now().minusDays(1));
-
-        Nachricht dummy3 = new Nachricht();
-        dummy3.setBenutzer("Personalabteilung");
-        dummy3.setTitel("Urlaubsantrag genehmigt");
-        dummy3.setVorschau("Ihr Urlaubsantrag wurde genehmigt...");
-        dummy3.setInhalt("""
-                Hallo,
-
-                Ihr Urlaubsantrag wurde genehmigt.
-
-                Wir wünschen Ihnen einen erholsamen Urlaub!
-
-                Mit freundlichen Grüßen
-                Personalabteilung
-                """);
-        dummy3.setEmpfangenAm(LocalDateTime.now().minusDays(5));
-        // Dummy-Daten Ende -----------------------------------------------------------------------------
-        
-        grid.setItems(List.of(dummy1, dummy2, dummy3));
+        grid.setItems(NachrichtService.getNachrichten());
 
         VerticalLayout details = new VerticalLayout();
         details.setPadding(false);
---
+++ src/main/java/com/psyduck/myDesk/persistenz/NachrichtService.java
@@ -0,0 +1,73 @@
+package com.psyduck.myDesk.persistenz;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NachrichtService {
+
+	private static final List<Nachricht> nachrichten = new ArrayList<>();
+	
+	static {
+		// Dummy-Daten Anfang -----------------------------------------------------------------------------
+        Nachricht dummy1 = new Nachricht();
+        dummy1.setBenutzer("Max Mustermann");
+        dummy1.setTitel("Willkommen bei myDesk");
+        dummy1.setVorschau("Herzlich willkommen bei myDesk...");
+        dummy1.setInhalt("""
+                Hallo,
+
+                herzlich willkommen bei myDesk!
+
+                Wir freuen uns, dass Sie unser System nutzen.
+                Bei Fragen steht Ihnen der Support jederzeit zur Verfügung.
+
+                Viele Grüße
+                Ihr myDesk-Team
+                """);
+        dummy1.setEmpfangenAm(LocalDateTime.now().minusHours(2));
+
+        Nachricht dummy2 = new Nachricht();
+        dummy2.setBenutzer("IT-Support");
+        dummy2.setTitel("Passwort geändert");
+        dummy2.setVorschau("Ihr Passwort wurde erfolgreich geändert...");
+        dummy2.setInhalt("""
+                Guten Tag,
+
+                Ihr Passwort wurde erfolgreich geändert.
+
+                Sollten Sie diese Änderung nicht selbst vorgenommen haben,
+                wenden Sie sich bitte umgehend an den IT-Support.
+                """);
+        dummy2.setEmpfangenAm(LocalDateTime.now().minusDays(1));
+
+        Nachricht dummy3 = new Nachricht();
+        dummy3.setBenutzer("Personalabteilung");
+        dummy3.setTitel("Urlaubsantrag genehmigt");
+        dummy3.setVorschau("Ihr Urlaubsantrag wurde genehmigt...");
+        dummy3.setInhalt("""
+                Hallo,
+
+                Ihr Urlaubsantrag wurde genehmigt.
+
+                Wir wünschen Ihnen einen erholsamen Urlaub!
+
+                Mit freundlichen Grüßen
+                Personalabteilung
+                """);
+        dummy3.setEmpfangenAm(LocalDateTime.now().minusDays(5));
+        // Dummy-Daten Ende -----------------------------------------------------------------------------
+        
+        nachrichten.add(dummy1);
+        nachrichten.add(dummy2);
+        nachrichten.add(dummy3);
+	}
+	
+	public static List<Nachricht> getNachrichten() {
+        return nachrichten;
+    }
+
+    public static int getAnzahlNachrichten() {
+        return nachrichten.size();
+    }
+}
