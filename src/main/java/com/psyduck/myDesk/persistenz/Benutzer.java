4a7d67d3351117b90cac095d7b39e71f7b148844
NikW5 <jannikwendler@web.de>
2026-07-31 19:17:20 +0200

    Issue #17 - Test-User angelegt, Validierung

---
+++ src/main/java/com/psyduck/myDesk/persistenz/Benutzer.java
@@ -0,0 +1,26 @@
+package com.psyduck.myDesk.persistenz;
+
+public class Benutzer {
+
+    private final String email;
+    private final String passwort;
+    private final String name;
+
+    public Benutzer(String email, String passwort, String name) {
+        this.email = email;
+        this.passwort = passwort;
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPasswort() {
+        return passwort;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
