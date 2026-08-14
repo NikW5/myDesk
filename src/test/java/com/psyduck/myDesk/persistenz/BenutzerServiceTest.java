449cc58dceb4c5f371abc2d2a540c084ea177ced
NikW5 <jannikwendler@web.de>
2026-08-14 19:37:50 +0200

    Issue #36 - Unittests fürs Login

---
+++ src/test/java/com/psyduck/myDesk/persistenz/BenutzerServiceTest.java
@@ -0,0 +1,59 @@
+package com.psyduck.myDesk.persistenz;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class BenutzerServiceTest {
+
+    @Test
+    void anmelden_gibtTrueBeiBestehendemBenutzerIAahZurueck() { 
+        boolean ergebnis = BenutzerService.anmelden(
+                "i-aah@test.de",
+                "1234"
+        );
+
+        assertTrue(ergebnis);
+    }
+
+    @Test
+    void anmelden_gibtTrueBeiBestehendemBenutzerEntonZurueck() {
+        boolean ergebnis = BenutzerService.anmelden(
+                "enton@test.de",
+                "abcd"
+        );
+
+        assertTrue(ergebnis);
+    }
+
+    @Test
+    void anmelden_gibtFalseBeiUnbekanntemBenutzerZurueck() {
+        boolean ergebnis = BenutzerService.anmelden(
+                "unbekannt@test.de",
+                "1234"
+        );
+
+        assertFalse(ergebnis);
+    }
+
+    @Test
+    void anmelden_gibtFalseBeiFalschemPasswortZurueck() {
+        boolean ergebnis = BenutzerService.anmelden(
+                "i-aah@test.de",
+                "falschesPasswort"
+        );
+
+        assertFalse(ergebnis);
+    }
+
+    @Test
+    void anmelden_gibtFalseBeiLeeremBenutzernamenZurueck() {
+        boolean ergebnis = BenutzerService.anmelden(
+                "",
+                "1234"
+        );
+
+        assertFalse(ergebnis);
+    }
+}
